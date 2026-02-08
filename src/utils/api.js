41cdc9a2034b2eded062620cc1d44677c743c38f
benzeroualaossama61 <benzeroualaossama61@gmail.com>
2026-02-08 20:08:51 +0100

    initial frontend commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // frontend/src/utils/api.js
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api" || "https://backend-linker.onrender.com/api";
 
 export default API_URL;
