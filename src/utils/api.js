d720ee9d0fbd60aaa3595e3f18a97a9c21f37ed0
benzeroualaossama61 <benzeroualaossama61@gmail.com>
2026-02-10 01:47:29 +0100

    dernier version du frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,6 @@
 // frontend/src/utils/api.js
-const API_URL = import.meta.env.VITE_API_URL || "https://backend-app-linker.onrender.com/api" || "http://localhost:5000/api" ||"web-production-34f0.up.railway.app/api";
+// Si VITE_API_URL est défini dans .env, on l'utilise
+// Sinon, si on est en développement, on utilise localhost
+// Sinon (production), on utilise l'URL de déploiement (Render ou Railway)
+const API_URL = import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://backend-app-linker.onrender.com/api");
 export default API_URL;
