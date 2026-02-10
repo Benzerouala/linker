afd685adddb09ebd36dae0aef2533bc283ec2524
benzeroualaossama61 <benzeroualaossama61@gmail.com>
2026-02-10 13:01:43 +0100

    linker

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,5 @@
 // Si VITE_API_URL est défini dans .env, on l'utilise
 // Sinon, si on est en développement, on utilise localhost
 // Sinon (production), on utilise l'URL de déploiement (Render ou Railway)
-const API_URL = import.meta.env.VITE_API_URL || "https://backend-app-linker.onrender.com/api";
+const API_URL = "https://backend-app-linker.onrender.com/api";
 export default API_URL;
